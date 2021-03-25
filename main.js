4da61ddcbaf3d7a53e6d02a812f0aa6fc74db1d0
MoranGuy <81372507+MoranGuy@users.noreply.github.com>
2021-03-25 16:56:33 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,196 @@
+const radioButtons = document.querySelectorAll('.radio')
+const generateButton = document.querySelector('.btn')
+const colorDisplay = document.querySelector('.display-text')
+const copiedText = document.querySelector('.copied-text')
+const CSS_COLOR_NAMES = [
+  'AliceBlue',
+  'AntiqueWhite',
+  'Aqua',
+  'Aquamarine',
+  'Azure',
+  'Beige',
+  'Bisque',
+  'Black',
+  'BlanchedAlmond',
+  'Blue',
+  'BlueViolet',
+  'Brown',
+  'BurlyWood',
+  'CadetBlue',
+  'Chartreuse',
+  'Chocolate',
+  'Coral',
+  'CornflowerBlue',
+  'Cornsilk',
+  'Crimson',
+  'Cyan',
+  'DarkBlue',
+  'DarkCyan',
+  'DarkGoldenRod',
+  'DarkGray',
+  'DarkGrey',
+  'DarkGreen',
+  'DarkKhaki',
+  'DarkMagenta',
+  'DarkOliveGreen',
+  'DarkOrange',
+  'DarkOrchid',
+  'DarkRed',
+  'DarkSalmon',
+  'DarkSeaGreen',
+  'DarkSlateBlue',
+  'DarkSlateGray',
+  'DarkSlateGrey',
+  'DarkTurquoise',
+  'DarkViolet',
+  'DeepPink',
+  'DeepSkyBlue',
+  'DimGray',
+  'DimGrey',
+  'DodgerBlue',
+  'FireBrick',
+  'FloralWhite',
+  'ForestGreen',
+  'Fuchsia',
+  'Gainsboro',
+  'GhostWhite',
+  'Gold',
+  'GoldenRod',
+  'Gray',
+  'Grey',
+  'Green',
+  'GreenYellow',
+  'HoneyDew',
+  'HotPink',
+  'IndianRed',
+  'Indigo',
+  'Ivory',
+  'Khaki',
+  'Lavender',
+  'LavenderBlush',
+  'LawnGreen',
+  'LemonChiffon',
+  'LightBlue',
+  'LightCoral',
+  'LightCyan',
+  'LightGoldenRodYellow',
+  'LightGray',
+  'LightGrey',
+  'LightGreen',
+  'LightPink',
+  'LightSalmon',
+  'LightSeaGreen',
+  'LightSkyBlue',
+  'LightSlateGray',
+  'LightSlateGrey',
+  'LightSteelBlue',
+  'LightYellow',
+  'Lime',
+  'LimeGreen',
+  'Linen',
+  'Magenta',
+  'Maroon',
+  'MediumAquaMarine',
+  'MediumBlue',
+  'MediumOrchid',
+  'MediumPurple',
+  'MediumSeaGreen',
+  'MediumSlateBlue',
+  'MediumSpringGreen',
+  'MediumTurquoise',
+  'MediumVioletRed',
+  'MidnightBlue',
+  'MintCream',
+  'MistyRose',
+  'Moccasin',
+  'NavajoWhite',
+  'Navy',
+  'OldLace',
+  'Olive',
+  'OliveDrab',
+  'Orange',
+  'OrangeRed',
+  'Orchid',
+  'PaleGoldenRod',
+  'PaleGreen',
+  'PaleTurquoise',
+  'PaleVioletRed',
+  'PapayaWhip',
+  'PeachPuff',
+  'Peru',
+  'Pink',
+  'Plum',
+  'PowderBlue',
+  'Purple',
+  'RebeccaPurple',
+  'Red',
+  'RosyBrown',
+  'RoyalBlue',
+  'SaddleBrown',
+  'Salmon',
+  'SandyBrown',
+  'SeaGreen',
+  'SeaShell',
+  'Sienna',
+  'Silver',
+  'SkyBlue',
+  'SlateBlue',
+  'SlateGray',
+  'SlateGrey',
+  'Snow',
+  'SpringGreen',
+  'SteelBlue',
+  'Tan',
+  'Teal',
+  'Thistle',
+  'Tomato',
+  'Turquoise',
+  'Violet',
+  'Wheat',
+  'White',
+  'WhiteSmoke',
+  'Yellow',
+  'YellowGreen',
+]
+
+colorDisplay.onclick = function () {
+  document.execCommand('copy')
+}
+
+colorDisplay.addEventListener('copy', function (event) {
+  event.preventDefault()
+  if (event.clipboardData) {
+    event.clipboardData.setData('text/plain', colorDisplay.textContent)
+  }
+  copiedText.classList.remove('hidden')
+  setTimeout(() => copiedText.classList.add('hidden'), 1500)
+})
+
+const typeChecker = () => {
+  if (radioButtons[0].checked) {
+    KeywordGenerator()
+  } else {
+    HexGenerator()
+  }
+}
+
+generateButton.addEventListener('click', typeChecker)
+
+const HexGenerator = () => {
+  const symbols = 'ABCDEF0123456789'
+  let hexCode = '#'
+  let i = 0
+  while (i < 6) {
+    hexCode += symbols[Math.floor(Math.random() * symbols.length)]
+    i++
+  }
+  document.body.style.backgroundColor = hexCode
+  colorDisplay.innerHTML = hexCode
+}
+
+const KeywordGenerator = () => {
+  let keyword =
+    CSS_COLOR_NAMES[Math.floor(Math.random() * CSS_COLOR_NAMES.length)]
+  document.body.style.backgroundColor = keyword
+  colorDisplay.innerHTML = keyword
+}
